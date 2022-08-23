90d41ae9999f8b8434b609b2be6f6be1664a5335
dev <dev@example.com>
2022-08-23 17:45:33 +0530

    nothing

--- src/Components/ApiIntegration.js
+++ src/Components/ApiIntegration.js
@@ -4,7 +4,6 @@ import { useState } from "react";
 import Table from "./Table";
 
 const ApiIntegration = () => {
-  console.log("hello this is new");
   const [result, setResult] = useState([]);
   const [allResults, setAllResults] = useState([]);
   const [length, setLength] = useState(0);
